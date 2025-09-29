73752a65fb5448956cd41119ca832cd32368bcfb
harsh-barman <harshbarman04@gmail.com>
2025-09-29 08:45:41 +0530

    Restore lost code from previous commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HeroSection from './components/herosection/herosection'
+import HeroSection from './components/pages/herosection/herosection'
+import Navbar from './components/Layout/Navbar'
 
 export default function App() {
   return (
     <Router>
+       
+      <Navbar/>
       <Routes>
         <Route path="/" element={<HeroSection />} />
         {/* Future routes like /about, /projects */}
---
+++ src/components/pages/herosection/herosection.jsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Lenis from '@studio-freight/lenis';
+import { GoArrowDownRight } from "react-icons/go";
+import CircularText from '../../styles/CircularText/CircularText'
+import HeroText from '../HeroText/HeroText';
+
+// Register ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+const HeroSection = () => {
+  const experienceRef = useRef(null);
+  const middleTextRef = useRef(null);
+  
+
+  useEffect(() => {
+    // Lenis smooth scroll setup
+    const lenis = new Lenis({
+      duration: 0.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: true,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    // lenisRef.current = lenis;
+
+
+
+    // Connect Lenis with GSAP ScrollTrigger
+  lenis.on('scroll', ScrollTrigger.update);
+
+   const updateLenis = (time) => lenis.raf(time * 1000);
+   gsap.ticker.add(updateLenis);
+
+    gsap.ticker.lagSmoothing(0);
+
+    const exp = gsap.timeline({
+      scrollTrigger: {
+        trigger: experienceRef.current,
+        start: 'top top',
+        end: '+=5000',
+        scrub: 1,
+        pin: experienceRef.current,
+        anticipatePin: 1,
+      },
+    });
+    gsap.set(middleTextRef.current, {
+      scaleX: 0.7,
+      scaleY: 3,
+    });
+    // Animation for scaling the middle text
+    exp.to(middleTextRef.current, {
+      scale: 60,
+      transformOrigin: 'center center',
+      ease: 'none',
+    });
+
+    // Cleanup function
+    return () => {
+      gsap.ticker.remove(updateLenis);
+       ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* Hero Section */}
+      <div
+        ref={experienceRef}
+        className="relative w-full h-screen overflow-hidden bg-gray-500  "
+      >
+        {/* Centered main content */}
+        <div className=" absolute inset-0  flex flex-col  md:gap-25 gap-8 text-right items-center justify-center w-full">
+          <h1
+            ref={middleTextRef}
+            className="font-black whitespace-nowrap select-none tracking-tight transform origin-center 
+                  font-mulish bg-[linear-gradient(45deg,#f0d1d6,#db98a3,#b86981,#7a4959,#3d252c,#5d0223,#8b0a3a,#b86981,#db98a3,#f0d1d6)]
+                  bg-[length:300%_300%] bg-clip-text text-transparent animate-waveGradient text-[clamp(3rem,12vw,14rem)]"
+          >
+            HARSH BARMAN
+          </h1>
+          
+          {/* Subtitle positioned below h1 */}
+          <div className="flex flex-row  md:justify-end justify-center gap-4 w-full md:pr-57  ">
+            <GoArrowDownRight className="md:text-3xl text-md text-gray-800" />
+            <h2 className="md:text-4xl text-lg  text-gray-800 ">
+              FRONTEND DEVELOPER
+            </h2>
+          </div>
+          <CircularText
+            text=" * SCROLL TO DISCOVER * SCROLL TO DISCOVER"
+            onHover="speedUp"
+            spinDuration={20}
+            className="absolute md:bottom-10 md:left-10 bottom-3 left-3"
+          />
+        </div>
+      </div>
+
+      {/* Next section for scroll effect */}
+      <section className="h-auto w-full  bg-[#1f1f1f]">
+        <div className=" ">
+          < HeroText className ='pt-20'/>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HeroSection;
